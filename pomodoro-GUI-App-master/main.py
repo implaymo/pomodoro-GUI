85b7e7098701049641311741877bf25ff11a839e
implaymo <136201112+implaymo@users.noreply.github.com>
2023-10-21 18:26:37 +0100

    Added text modification when is work time and break time

--- pomodoro-GUI-App-master/main.py
+++ pomodoro-GUI-App-master/main.py
@@ -7,10 +7,14 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
-SHORT_BREAK_MIN = 20
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+TIMER_HEADER = "TIMER"
+WORK_HEADER = "WORK"
+BREAK_HEADER = "BREAK"
+
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
@@ -23,25 +27,18 @@ def start_timer():
     work_time_secs = WORK_MIN * 60
     short_break_secs = SHORT_BREAK_MIN * 60
     long_break_secs = LONG_BREAK_MIN * 60
-    time = 0
     # Checks in which rep the timer is to switch amount of time
     if reps in [0, 2, 4, 6]:
         count_down(work_time_secs)
-        time = work_time_secs
-        reps += 1
+        canvas.itemconfig(timer_text, text=WORK_HEADER, fill=GREEN, font=(FONT_NAME, 50, "bold"))
     elif reps in [1, 3, 5]:
         count_down(short_break_secs)
-        time = short_break_secs
-        reps += 1
+        canvas.itemconfig(timer_text, text=BREAK_HEADER, fill=PINK, font=(FONT_NAME, 50, "bold"))
     elif reps in [7]:
         count_down(long_break_secs)
-        time = long_break_secs
+        canvas.itemconfig(timer_text, text=BREAK_HEADER, fill=PINK, font=(FONT_NAME, 50, "bold"))
         reps = 0
-
-    # Keeps timer running till it reaches the 8th time
-    if reps < 8:
-        milliseconds = int(time * 1000)
-        root.after(milliseconds, start_timer)
+    reps += 1
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -52,10 +49,12 @@ def count_down(seconds):
     count_min = math.floor(seconds / 60)
     count_sec = seconds % 60
     # Setups timer to work like 00:00
-    canvas.itemconfig(timer, text=f"{count_min}:{count_sec:02}")
+    canvas.itemconfig(timer, text=f"{count_min:02}:{count_sec:02}")
     # Decreases time in timer
     if seconds > 0:
         root.after(1000, count_down, seconds - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -63,17 +62,17 @@ def count_down(seconds):
 # Window
 root = Tk()
 root.title("Pomodoro timer")
-root.config(padx=100, pady=50, bg=PINK)
+root.config(padx=100, pady=50, bg=YELLOW)
 
 # Image setup
-canvas = Canvas(root, width=400, height=500, bg=PINK, highlightthickness=0)
+canvas = Canvas(root, width=400, height=500, bg=YELLOW, highlightthickness=0)
 canvas.grid(column=2, row=2, rowspan=2)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(200, 250, image=tomato_img)
 
 # Text
 timer = canvas.create_text(200, 250, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
-timer_text = canvas.create_text(200, 100, text="TIMER", fill=GREEN, font=(FONT_NAME, 50, "bold"))
+timer_text = canvas.create_text(200, 100, text=TIMER_HEADER, fill=GREEN, font=(FONT_NAME, 50, "bold"))
 check_mark = canvas.create_text(200, 400, text="✔️", fill=GREEN, font=(FONT_NAME, 10, "bold"))
 
 # Start button
