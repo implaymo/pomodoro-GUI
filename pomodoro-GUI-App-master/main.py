ff83a8389b494feaaf7f1ea677bdd250ee67b2d1
implaymo <136201112+implaymo@users.noreply.github.com>
2023-10-21 18:42:43 +0100

    Added check_mark when its work time to keep track how many work moments user had.

--- pomodoro-GUI-App-master/main.py
+++ pomodoro-GUI-App-master/main.py
@@ -10,10 +10,13 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-reps = 0
+REPS = 0
 TIMER_HEADER = "TIMER"
 WORK_HEADER = "WORK"
 BREAK_HEADER = "BREAK"
+X_CHECK_MARK = 180
+Y_CHECK_MARK = 400
+
 
 
 
@@ -23,22 +26,25 @@ BREAK_HEADER = "BREAK"
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 def start_timer():
-    global reps
+    global REPS
+    global X_CHECK_MARK
     work_time_secs = WORK_MIN * 60
     short_break_secs = SHORT_BREAK_MIN * 60
     long_break_secs = LONG_BREAK_MIN * 60
     # Checks in which rep the timer is to switch amount of time
-    if reps in [0, 2, 4, 6]:
+    if REPS in [0, 2, 4, 6]:
         count_down(work_time_secs)
         canvas.itemconfig(timer_text, text=WORK_HEADER, fill=GREEN, font=(FONT_NAME, 50, "bold"))
-    elif reps in [1, 3, 5]:
+        canvas.create_text(X_CHECK_MARK, Y_CHECK_MARK, text="✔️", fill=GREEN, font=(FONT_NAME, 10, "bold"))
+        X_CHECK_MARK += 20
+    elif REPS in [1, 3, 5]:
         count_down(short_break_secs)
         canvas.itemconfig(timer_text, text=BREAK_HEADER, fill=PINK, font=(FONT_NAME, 50, "bold"))
-    elif reps in [7]:
+    elif REPS in [7]:
         count_down(long_break_secs)
-        canvas.itemconfig(timer_text, text=BREAK_HEADER, fill=PINK, font=(FONT_NAME, 50, "bold"))
-        reps = 0
-    reps += 1
+        canvas.itemconfig(timer_text, text=BREAK_HEADER, fill=RED, font=(FONT_NAME, 50, "bold"))
+        REPS = 0
+    REPS += 1
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -73,7 +79,6 @@ canvas.create_image(200, 250, image=tomato_img)
 # Text
 timer = canvas.create_text(200, 250, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 timer_text = canvas.create_text(200, 100, text=TIMER_HEADER, fill=GREEN, font=(FONT_NAME, 50, "bold"))
-check_mark = canvas.create_text(200, 400, text="✔️", fill=GREEN, font=(FONT_NAME, 10, "bold"))
 
 # Start button
 start_button = Button(text="Start", command=start_timer)
